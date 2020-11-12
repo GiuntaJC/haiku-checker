4318b6b3d7dccc007dc5da85eb437ae472025848
Chelsea Becker <cschweig2@gmail.com>
2020-11-12 14:04:07 -0800

    add 3 packages for syllable() functionality
    Co-authored-by: Agata Kolodziej <agatakolohe@gmail.com>

--- src/haiku.js
+++ src/haiku.js
@@ -1,11 +1,18 @@
+var cmu = require('cmu-pronouncing-dictionary');
+const extractwords = require('extractwords');
+const syllables = require('syllables');
+
 export default class Haiku {
   constructor(line1, line2, line3) {
     this.line1 = line1;
     this.line2 = line2;
     this.line3 = line3;
   }
-vowelCount() {
-  return this.line1.match(/[aeiou]/gi).length;
-}
-  
+  vowelCount() {
+    return this.line1.match(/[aeiou]/gi).length;
+  }
+  findSyllable() {
+    let syllableCount = syllables(this.line2);
+    return syllableCount;
+  }
 };
